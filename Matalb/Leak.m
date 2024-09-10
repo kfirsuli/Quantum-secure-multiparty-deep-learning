6bc9fb43acb50cd44c85e4163f2f313daa9144bc
kfirsuli <84624863+kfirsuli@users.noreply.github.com>
2024-09-10 20:38:11 +0300

    Delete Matalb directory

--- Matalb/Leak.m
+++
@@ -1,7 +0,0 @@
-function [L] = Leak(V, T,epsilon)
-a = V; b = T.*(V-1)+1+epsilon; c = sqrt(T.*(V.^2-1));
-z = sqrt((a+b).^2-4*c.^2);
-v1 = 0.5*(z+b-a); v2 = 0.5*(z-(b-a)); v3 = b-c.^2./(a+1);
-% L = G(v3);
-L = G(v1)+G(v2)-G(v3);
-end
